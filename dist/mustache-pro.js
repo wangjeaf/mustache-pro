225e5e3a6ceb4b12dbc819301b78a6ae7b6939c7
思竹 <zhifu.wzf@alibaba-inc.com>
2015-09-28 15:33:58 +0800

    v

--- dist/mustache-pro.js
+++ dist/mustache-pro.js
@@ -1,4 +1,4 @@
-/*! mustache-pro - v0.2.3 - 2015-09-28 *//*!
+/*! mustache-pro - v0.2.4 - 2015-09-28 *//*!
  * mustache.js - Logic-less {{mustache}} templates with JavaScript
  * http://github.com/janl/mustache.js
  */
